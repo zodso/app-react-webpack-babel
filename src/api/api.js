00b2d6423318ddf580fca3069ded16afff76b734
Daniele Garito <daniele.garito@gmail.com>
2019-05-09 20:01:06 +0200

    fix https

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,10 @@
+let protocol = window.location.protocol == "http:" ? "http:" : "https:";
 export let api = {
-  baseUri: "http://api.themoviedb.org/3/",
+  baseUri: protocol + "//api.themoviedb.org/3/",
   key: "556ce776b643d217adb35e38d7b68d86",
   listUri: "genre/movie/list",
   discoverUri: "discover/movie",
-  imgesUri: "http://image.tmdb.org/t/p/",
+  imgesUri: protocol + "//image.tmdb.org/t/p/",
   lang: "en-US"
 };
 
