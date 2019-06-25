e72ba392026e80d3a05f84c9bc2fa9971a4f0efa
Daniele Garito <daniele.garito@gmail.com>
2019-06-25 10:19:06 +0200

    hide api key

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 let protocol = window.location.protocol == "http:" ? "http:" : "https:";
 export let api = {
   baseUri: protocol + "//api.themoviedb.org/3/",
-  key: "556ce776b643d217adb35e38d7b68d86",
+  key: "<<Your_api_key>>",
   listUri: "genre/movie/list",
   discoverUri: "discover/movie",
   imgesUri: protocol + "//image.tmdb.org/t/p/",
--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,9 +1,9 @@
-export const API_KEY = "AIzaSyCHDZ9WQf6kHR3pY65nweTYPyDQxKa-AzA";
+export const API_KEY = "<<Your_api_key>>";
 export const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: "movies-api-with-react.firebaseio.com",
-  databaseURL: "https://movies-api-with-react.firebaseio.com",
-  projectId: "movies-api-with-react",
-  storageBucket: "movies-api-with-react.appspot.com",
-  messagingSenderId: "277860574039"
+  authDomain: "<<Your_auth_domain>>",
+  databaseURL: "<<Your_base_url>>",
+  projectId: "<<Your_project>>",
+  storageBucket: "<<Your_storage>>",
+  messagingSenderId: ""
 };
